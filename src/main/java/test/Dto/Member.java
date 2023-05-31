9fc7d92430a1e9a55c3ac93579c353072f61b1b9
JMKooZ <ck03412@naver.com>
2023-05-31 21:37:18 +0900

    기존에 만들어놓았던 프로젝트 강의보며 수정

--- src/main/java/test/Dto/Member.java
+++ src/main/java/test/Dto/Member.java
@@ -14,11 +14,37 @@ public class Member {
 
     public Member() {
     }
+
     public Member(int id, String name, int money) {
         this.id = id;
         this.name = name;
         this.money = money;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
     public void deposit(int money){
         this.money += money;
     }
